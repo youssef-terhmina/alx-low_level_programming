e8608d7a09b2ac5eb19c8ca1e473282d427f0808
Your Name <you@example.com>
2023-03-30 17:30:35 -0700

    buffer

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -7,5 +7,3 @@ char *string_toupper(char *s);
 char *cap_string(char *s);
 char *leet(char *s);
 int _putchar(char c);
-void print_number(int n);
-char *infinite_add(char *n1, char *n2, char *r, int size_r);
