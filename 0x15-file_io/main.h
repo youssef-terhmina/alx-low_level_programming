d4d25373bc34b86177602330d98b7ed4f3e94672
youssef-terhmina <terhminayoussef@gmail.com>
2023-05-08 16:31:32 -0700

    up main

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -11,6 +11,7 @@ int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
 int _putchar(char c);
 int _strlen(const char *s);
+void errors(int statu, const char *message);
 
 #endif
 
