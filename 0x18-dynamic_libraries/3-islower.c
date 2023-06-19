e9d7ed24e57ff029be95f02fc0c6ed7be56e8c50
youssef-terhmina <terhminayoussef@gmail.com>
2023-06-19 11:24:40 -0700

    A library is not a luxury but one of the necessities of life

---
+++ 0x18-dynamic_libraries/3-islower.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _islower - Show 1 if input is a.
+ * lowercase character. Another cases, shows
+ * 0
+ *
+ * @c: The character in ASCII code
+ *
+ * Return: 1 for lowercase character. 0for the rest.
+ */
+int _islower(int c)
+{
+	if (c >= 97 && c <= 122)
+	{
+		return (1);
+
+	}
+	else
+	{
+		return (0);
+
+	}
+	_putchar('\n');
+
+}
---
+++ 0x18-dynamic_libraries/4-isalpha.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _isalpha - Show 1 if the input is a.
+ * letter Anothercases, show 0
+ *
+ * @c: The character in ACII code
+ *
+ * Return: 1 for letters. 0 for the rest.
+ */
+int _isalpha(int c)
+{
+	if ((c >= 97 && c <= 122) || (c >= 65 && c <= 90))
+	{
+		return (1);
+
+	}
+	else
+	{
+		return (0);
+	}
+	_putchar('\n');
+}
