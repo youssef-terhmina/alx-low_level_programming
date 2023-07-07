5358c04eca44e26a2bdbc4b6cfdfd8de47e2c797
youssef-terhmina <terhminayoussef@gmail.com>
2023-07-06 17:02:16 -0700

    #

---
+++ 0x1A-hash_tables/2-key_index.c
@@ -0,0 +1,14 @@
+#include "hash_tables.h"
+
+/**
+ * key_index - Get the index for storing a key in a hash table.
+ * @key: The key to get the index of.
+ * @size: The size of the hash table array.
+ * Return: The index of the key.
+ * Description: Uses the djb2 algorithm for index computation.
+ */
+
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+	return (hash_djb2(key) % size);
+}
